103f1521d73b2ced9983a679c3e43dd03a1e37b2
SatooRu65536 <ryuto4619@gmail.com>
2023-04-13 23:48:10 +0900

    04-13 追加

---
+++ 2023-04-13/src/01_01_helloWorld.c
@@ -0,0 +1,15 @@
+//
+//  01_01_helloWorld.c
+//  prg1K
+//
+//  Created by k23075 on 2023/04/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+  printf("Hello, World!\n");
+  printf("Tada Ryuto\n");
+
+  return 0;
+}
---
+++ 2023-04-13/src/01_issue1_k23075.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("                         ............\n");
+  printf("                     ..+MMMMMMMMMMMMMMMNa,.\n");
+  printf("                  ..MMMMMMMMMMMMMMMMMMMMMMMNa.\n");
+  printf("                .dMMMMMMMMMMMMMMMMMMMMMMMMMMMMN,\n");
+  printf("              .dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN,\n");
+  printf("             .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm.\n");
+  printf("           .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN,\n");
+  printf("          .MMMMMMMMMMMMMM`MMMMMMMMMMMM`HMMMMMMMMMMMMMx\n");
+  printf("         .MMMMMMMMMMMMM'uMm,MMMMMMMM％(MN,MMMMMMMMMMMMMp\n");
+  printf("        .MMMMMMMMMMMMMM,TM`.MMMMMMMMp!HD.MMMMMMMMMMMMMMp\n");
+  printf("       .MMMMMMMMMMMMMMMMMMMMM8!!!!7MMMMMMMMMMMMMMMMMMMMMx\n");
+  printf("      .MMMMMMMMYMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMUMMMMMMMM,\n");
+  printf("      dMMMMMMMM`  .TWMMMMMMMMMMMMMMMMMMMMMM#`'   MMMMMMMMN \n");
+  printf("     .MMMMMMMMF        !TWMMMMMMMMMMMMW`=        dMMMMMMMM]\n");
+  printf("     MMMMMMMMM]               _!!!               .MMMMMMMMM.\n");
+  printf("    .MMMMMMMMM:                                  .MMMMMMMMMb\n");
+  printf("    MMMMMMMMMM                                    MMMMMMMMMM.\n");
+  printf("   .MMMMMMMMM#                                    dMMMMMMMMM]\n");
+  printf("   gMMMMMMMMMF                                    JMMMMMMMMM# \n");
+  printf("   MMMMMMMMMM]                                    ,MMMMMMMMMM.\n");
+  printf("  .MMMMMMMMMM}                                    .MMMMMMMMMM[\n");
+  printf("  .MMMMMMMMMM                                      MMMMMMMMMM]\n");
+  printf("  JMMMMMMMMM#                                      dMMMMMMMMM@\n");
+  printf("  dMMMMMMMMMF                                      -MMMMMMMMM#\n");
+  printf("  dMMMMMMMMM`                                       MMMMMMMMM#\n");
+  printf("  dMMMMMMMMF                                        JMMMMMMMM#\n");
+  printf("  gMMMMMMMM`                                         MMMMMMMM#\n");
+  printf("  (MMMMMMM^                                          .MMMMMMMF\n");
+  printf("  .MMMMMB`                                             TMMMMM/\n");
+  printf("   dMM``                                                 7MM#\n");
+  printf("   .MMb                                                  .MM％\n");
+  printf("    !MMp                                                .MMF\n");
+  printf("     TMMp                                              .MMF\n");
+  printf("      !MMh.                                          .JMMD\n");
+  printf("       .WMMa.                                      ..MMH^\n");
+  printf("         (HMMN,.                                 .dMMM=\n");
+  printf("          .MMMMMMa..                        ..+MMMMMMp\n");
+  printf("         .M#::<TWMMMMMMNggJ...........JgNMMMMMM#5>::MM[\n");
+  printf("          TMMNP::::<7TMMMMMMMMMMMMMMMMMMMMB=<::::jNMMB\n");
+  printf("            .MNggMe:+MM3                ,MNJ:+Ng+MM^\n");
+  printf("              !``TMMM#!                  .TMMMB```\n");
+
+  return 0;
+}
