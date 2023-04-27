d4282801adcc635cfe9ef8a8455cae83d08f8aec
SatooRu65536 <ryuto4619@gmail.com>
2023-04-27 12:28:17 +0900

    修正

--- 2023-04-27/src/03_07_rectangle.c
+++ 2023-04-27/src/03_07_rectangle.c
@@ -5,8 +5,8 @@ int main(int argc, const char* argv[]) {
 
   printf("a b? ");
   scanf("%d %d", &a, &b);
-  printf("perimeter = %d", a * 2 + b * 2);
-  printf("area = %d", a * b);
+  printf("perimeter = %d\n", a * 2 + b * 2);
+  printf("area = %d\n", a * b);
 
   return 0;
 }
