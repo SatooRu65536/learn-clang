275e333204a7d4be7f3d0aa020e4b663461780a1
SatooRu65536 <ryuto4619@gmail.com>
2023-05-18 09:45:34 +0900

    0518 完了

---
+++ 2023-05-18/src/05_02_if2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b;
+
+  printf("a? ");
+  scanf("%d", &a);
+  printf("b? ");
+  scanf("%d", &b);
+
+  if (a == b) printf("aとbの値は等しいです\n");
+
+  printf("計算終了\n");
+  return 0;
+}
