b41e4fd87c473ff9aa456466ff7379602cd44549
SatooRu65536 <ryuto4619@gmail.com>
2023-06-22 09:34:44 +0900

    第10回 完了

---
+++ 2023-06-22/src/10_02_arrayinput.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int heights[5] = {0};
+  double sum = 0;
+
+  for (int i = 0; i < 5; i++) {
+    printf("%d人目の身長? ", i + 1);
+    scanf("%d", &heights[i]);
+    sum += (double)heights[i];
+  }
+
+  double avg = sum / 5;
+  printf("平均身長 = %f\n", avg);
+
+  return 0;
+}
