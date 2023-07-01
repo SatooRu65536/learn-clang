97b292925eb3f4db03cf9d9467dc95b0ec0dd545
SatooRu65536 <ryuto4619@gmail.com>
2023-07-02 00:05:22 +0900

    途中

--- final_assignment/main.c
+++ final_assignment/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define PNG_SIGNATURE_SIZE 8
+#include <string.h>
 
 typedef struct {
   uint32_t width;
@@ -19,47 +18,92 @@ typedef struct {
 } Image;
 
 int check_png_signature(FILE* file) {
-  unsigned char png_signature[PNG_SIGNATURE_SIZE] = {137, 80, 78, 71,
-                                                     13,  10, 26, 10};
-  unsigned char file_signature[PNG_SIGNATURE_SIZE];
+  unsigned char png_signature[] = {0x89, 0x50, 0x4E, 0x47,
+                                   0x0D, 0x0A, 0x1A, 0x0A};
+  unsigned char file_signature[8];
 
-  fread(file_signature, 1, PNG_SIGNATURE_SIZE, file);
+  fread(file_signature, 1, 8, file);
 
-  for (int i = 0; i < PNG_SIGNATURE_SIZE; i++) {
+  for (int i = 0; i < 8; i++) {
     if (file_signature[i] != png_signature[i]) return 0;
   }
 
   return 1;
 }
 
-int read_png_header(FILE* file, PNGHeader* header) {
-  // 幅と高さの情報を読み取る
-  fread(&header->width, 4, 1, file);
-  fread(&header->height, 4, 1, file);
-
-  // ビット深度の情報を読み取る
-  int depth;
-  fread(&depth, 1, 1, file);
-  header->depth = depth;
-
-  // カラーモードの情報を読み取る
-  int color_type;
-  fread(&color_type, 1, 1, file);
-  header->color_type = color_type;
-
-  // その他の情報は読み取らない
-  fseek(file, 2, SEEK_CUR);
+int skip_until_ihdr(FILE* file) {
+  // 0x49, 0x48, 0x44, 0x52 が見つかるまで読み込む
+  unsigned char ihdr_signature[] = {0x49, 0x48, 0x44, 0x52};
+  unsigned char signature[4];
+  while (1) {
+    fread(signature, 1, 4, file);
+    if (signature[0] == ihdr_signature[0] &&
+        signature[1] == ihdr_signature[1] &&
+        signature[2] == ihdr_signature[2] &&
+        signature[3] == ihdr_signature[3]) {
+      break;
+    }
+  }
 
   return 1;
 }
 
+int skip_until_idat(FILE* file) {
+  // 0x49, 0x48, 0x44, 0x52 が見つかるまで読み込む
+  unsigned char ihat_signature[] = {0x49, 0x44, 0x41, 0x54};
+  unsigned char signature[4];
+  while (1) {
+    fread(signature, 1, 4, file);
+    if (signature[0] == ihat_signature[0] &&
+        signature[1] == ihat_signature[1] &&
+        signature[2] == ihat_signature[2] &&
+        signature[3] == ihat_signature[3]) {
+      break;
+    }
+  }
+
+  return 1;
+}
+
+void read_ihdr(FILE* file, Image* image) {
+  // 4バイトの幅、4バイトの高さ、1バイトのビット深度、1バイトのカラータイプを読み込む
+  unsigned char width[4];
+  unsigned char height[4];
+  unsigned char depth[1];
+  unsigned char color_type[1];
+
+  fread(width, 1, 4, file);
+  fread(height, 1, 4, file);
+  fread(depth, 1, 1, file);
+  fread(color_type, 1, 1, file);
+
+  int width_int = 0;
+  for (int i = 0; i < 4; i++) width_int += width[i] << (8 * (3 - i));
+  image->header.width = width_int;
+
+  int height_int = 0;
+  for (int i = 0; i < 4; i++) height_int += height[i] << (8 * (3 - i));
+  image->header.height = height_int;
+
+  image->header.depth = depth[0];
+
+  image->header.color_type = color_type[0];
+}
+
+void read_data(FILE* file, Image* image) {
+}
+
 void read_png(FILE* file, Image* image) {
-  // ヘッダー情報を解析してイメージの幅、高さ、カラーモードを取得する処理を実装する
-  read_png_header(file, &image->header);
+  // イメージの幅、高さ、カラーモードを取得する
+  read_ihdr(file, image);
   printf("width: %d\n", image->header.width);
   printf("height: %d\n", image->header.height);
   printf("depth: %d\n", image->header.depth);
   printf("color_type: %d\n", image->header.color_type);
+
+  skip_until_idat(file);
+
+  read_data(file, image);
 
   // データ部分を解凍（デコード）する処理を実装する
   // イメージデータを生成し、image構造体にセットする処理を実装する
@@ -79,6 +123,8 @@ int main() {
     fclose(file);
     return 1;
   }
+
+  skip_until_ihdr(file);
 
   Image image;
   read_png(file, &image);
